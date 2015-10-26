6896171a7c8c747812567428bb0c9759fdb6c07a
Adam Particka <aparticka@fbdfrozen.com>
2015-10-26 16:56:32 -0500

    Added tests

---
+++ test/createBaconComponent.spec.js
@@ -0,0 +1,52 @@
+import { createBaconComponent } from '../src/index';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import Bacon from 'baconjs';
+import { expect } from 'chai';
+
+function getRenderedComponentInContainer(Component, Container) {
+  const rendered = TestUtils.renderIntoDocument(
+    <Container pass='through' />
+  );
+  return TestUtils.findRenderedComponentWithType(rendered, Component);
+}
+
+describe('createBaconComponent', () => {
+  const Component = React.createClass({ render: () => <div /> });
+  const mapProps = () => Bacon.combineTemplate();
+  const render = props => <Component {...props} />;
+
+  it('should create a higher-order component if second argument undefined', () => {
+    const Container = createBaconComponent(mapProps);
+    expect(typeof Container).to.equal('function');
+    expect(Container.length).to.equal(1);
+  });
+  it('should create a React component if second argument defined', () => {
+    const Container = createBaconComponent(mapProps, render);
+    expect(typeof Container.prototype.render).to.equal('function');
+  });
+  it('should pass props through if shouldPassPropsThrough set to true', () => {
+    const Container = createBaconComponent(mapProps, render, true);
+    const component = getRenderedComponentInContainer(Component, Container);
+    expect(component.props.pass).to.equal('through');
+  });
+  it('should not pass props through if shouldPassPropsThrough set to false', () => {
+    const Container = createBaconComponent(mapProps, render);
+    const component = getRenderedComponentInContainer(Component, Container);
+    expect(component.props.pass).to.equal(undefined);
+  });
+  it('should pass props from Bacon Property to component', () => {
+    const Container = createBaconComponent(() => {
+      return Bacon.combineTemplate({ passed: Bacon.constant('fromBacon') });
+    }, render);
+    const component = getRenderedComponentInContainer(Component, Container);
+    expect(component.props.passed).to.equal('fromBacon');
+  });
+  it('should have props accessible in propsP', () => {
+    const Container = createBaconComponent(propsP => {
+      return propsP;
+    }, render);
+    const component = getRenderedComponentInContainer(Component, Container);
+    expect(component.props.pass).to.equal('through');
+  });
+});
